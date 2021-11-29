9df90466570f6297bf235778caa3aa224114404b
Maxime Maitrot <maitrot@et.esiea.fr>
2021-11-29 15:54:53 +0100

    avancement de la partie 5)

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.nio.file.*;
+import java.util.Arrays;
 
 public class Launcher {
     public static void main(String[] args){
@@ -6,6 +9,9 @@ public class Launcher {
         System.out.println("Bienvenue !");
         java.util.Scanner var = new java.util.Scanner(System.in);
         String Argument = var.nextLine();
+        Path fichierchemin;
+        String path;
+        String content;
         while (!Argument.equals("quit")){
             if (Argument.equals("fibo")){
                 int nbr = var.nextInt();
@@ -17,6 +23,37 @@ public class Launcher {
                 f0=0;
                 f1=1;
                 System.out.println(""+fn);
+            }
+            if (Argument.equals("freq")){
+                //todo des choses sombres
+                path = var.next();
+                fichierchemin = Paths.get(path);
+                try {
+                    content = Files.readString(fichierchemin);
+
+                    content = content.replaceAll(","," ");
+                    content = content.replaceAll("\\?"," ");
+                    content = content.replaceAll("\\!"," ");
+                    content = content.replaceAll("\\."," ");
+                    content = content.replaceAll("\\:", " ");
+                    content = content.replaceAll("\\;", " ");
+                    content = content.replaceAll("\\'"," ");
+                    content = content.replaceAll("  "," ");
+                    content = conten.replaceAll("\n"," ");
+                    content = content.toLowerCase();
+                    String[] contenttabs = content.split(" ");
+                    for(String contenttab : contenttabs){
+                        if (contenttab.isBlank()){
+                            contenttab = null;
+                        }
+                        System.out.println(contenttab);
+                    }
+                    Stream Streamcontent = stream(contenttabs);
+                    System.out.println(content);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
             }
             else {
                 System.out.println("Unknow Command");
