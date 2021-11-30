e4e6ea1eb2baf73dee2faae8a4670d9fefc7f799
Maxime Maitrot <maitrot@et.esiea.fr>
2021-11-30 15:28:00 +0100

    Part 6 done

--- src/Fibo.java
+++ src/Fibo.java
@@ -9,7 +9,8 @@ public class Fibo implements Command{
     @Override
     public boolean run(Scanner scanner) {
         int f0 = 0, f1 = 1,f2 = 0;
-        for (int i = 0; i < scanner.nextInt()+1 ; i++){
+        int nbr = scanner.nextInt();
+        for (int i = 2; i < nbr+1 ; i++){
             f2 = f0 + f1;
             f0 = f1;
             f1 = f2;
--- src/Launcher.java
+++ src/Launcher.java
@@ -1,22 +1,32 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args){
         int i;
         System.out.println("Bienvenue !");
-        Scanner var = new Scanner(System.in);
-        boolean Isrunning = true;
-        ArrayList<Command> CommandList = new ArrayList<Command>();
+
+        boolean Isrunning = false;
+        ArrayList<Command> CommandList = new ArrayList<>();
         CommandList.add(new Quit());
         CommandList.add(new Fibo());
         CommandList.add(new Freq());
+        int index = -1;
+        Scanner var = new Scanner(System.in);
         while (!Isrunning) {
             String UserEntry = var.next();
             for ( i = 0 ; i < CommandList.size(); i++){
-                if (UserEntry.toString().equals(CommandList.get(i).name())){
-                    
+                if (UserEntry.equals(CommandList.get(i).name())){
+                    index = i;
 
                 }
+            }
+            if (index == -1){
+                System.out.println("Unknow Command");
+                Isrunning = false;
+            }
+            else {
+                Isrunning = CommandList.get(index).run(var);
             }
 
         }
