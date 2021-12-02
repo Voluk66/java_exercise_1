cbefa12c813897f13b191d5502630ba5de40b713
Maxime Maitrot <maitrot@et.esiea.fr>
2021-12-02 18:16:41 +0100

    Correction

--- src/Fibo.java
+++ src/Fibo.java
@@ -9,6 +9,7 @@ public class Fibo implements Command{
     @Override
     public boolean run(Scanner scanner) {
         int f0 = 0, f1 = 1,f2 = 0;
+        scanner = new Scanner(System.in);
         int nbr = scanner.nextInt();
         for (int i = 2; i < nbr+1 ; i++){
             f2 = f0 + f1;
